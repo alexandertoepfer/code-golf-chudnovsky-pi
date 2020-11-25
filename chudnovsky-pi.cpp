e06b3d5423eea004e9904d6a212af0ad085a4ad2
Alexander Töpfer <74869642+alexandertoepfer@users.noreply.github.com>
2020-11-25 18:07:05 +0100

    fix Assignment of H and Power of -1 (305 Bytes -> 287 Bytes)

--- chudnovsky-pi.cpp
+++ chudnovsky-pi.cpp
@@ -5,7 +5,7 @@
  *    3    38       |  __/| |  | |-|||  __/|  __/|    /| \_/| /  \ | || |  ||| |-||  | |  | || \_/|| | \||
  *    3    27       \_/   \_/  \_/ \|\_/   \_/   \_/\_\\____//__/\\\_/\_/  \|\_/ \|  \_/  \_/\____/\_/  \|
  *    9    50 2     
- *  8 8    4197     Code Golf 100 Digits of Pi: C++17 in 305 Bytes 
+ *  8 8    4197     Code Golf 100 Digits of Pi: C++17 in 287 Bytes 
  *   16     93  SS
  * Alexander
  * Töpfer 2020
@@ -15,6 +15,7 @@
 #include <boost/multiprecision/cpp_dec_float.hpp>
 #include <boost/multiprecision/cpp_int.hpp>
 #define cpp_float cpp_dec_float_100
+using namespace std;
 using namespace boost::math;
 using namespace boost::multiprecision;
 /* Unfortunately had to implement a custom factorial function because boost exceeds file size limit
@@ -23,5 +24,5 @@ using namespace boost::multiprecision;
  * "T factorial(unsigned i, const Policy&); [with T = cpp_dec_float_100]"
  * Code from boost/math/special_functions/factorials.hpp
  */
-cpp_float A=13591409,B=545140134,C=640320,D=1.5,E=1,(*f)(cpp_int)=[](cpp_int n){if(n>1){cpp_int r=1;for(cpp_int i=1;i<=n;i++)r=r*i;return cpp_float(r);}return E;},G=0,H;
-int main(){for(int k=0;k<8;k++){G+=((k%2==1?-E:E)*f(6*k)*(A+B*k))/(f(3*k)*pow(f(k),3)*pow(C,3*k+D));}H=E/(12*G);std::cout<<H.str(100);}
+cpp_float A=13591409,B=545140134,C=640320,D=1.5,E=1,(*f)(cpp_int)=[](cpp_int n){if(n<2)return E;cpp_int r=1,i=1;for(;i<=n;i++)r=r*i;return cpp_float(r);},G=0,H;
+int main(){for(int k=0;k<8;k++){G+=(pow(-1,k)*f(6*k)*(A+B*k))/(f(3*k)*pow(f(k),3)*pow(C,3*k+D));}cout<<(H=E/(12*G)).str(100);}
