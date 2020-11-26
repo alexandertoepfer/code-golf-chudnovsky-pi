3e97a345cabe44f31033f4e26a973281ce541514
Alexander Töpfer <74869642+alexandertoepfer@users.noreply.github.com>
2020-11-26 08:21:54 +0100

    remove Constants (227 -> 219 Bytes)

--- chudnovsky-pi.cpp
+++ chudnovsky-pi.cpp
@@ -1,4 +1,3 @@
-
 /*     3.141592
  *    653589793      ____  _    ____  ____  ____  ____  ____ ___  _ _  _      ____  _____  _  ____  _     
  *   23    84       /  __\/ \  /  _ \/  __\/  __\/  __\/  _ \\  \/// \/ \__/|/  _ \/__ __\/ \/  _ \/ \  /|
@@ -6,7 +5,7 @@
  *    3    38       |  __/| |  | |-|||  __/|  __/|    /| \_/| /  \ | || |  ||| |-||  | |  | || \_/|| | \||
  *    3    27       \_/   \_/  \_/ \|\_/   \_/   \_/\_\\____//__/\\\_/\_/  \|\_/ \|  \_/  \_/\____/\_/  \|
  *    9    50 2     
- *  8 8    4197     Code Golf 100 Digits of Pi: C++17 in 227 Bytes 
+ *  8 8    4197     Code Golf 100 Digits of Pi: C++17 in 219 Bytes 
  *   16     93  SS
  * Alexander
  * Töpfer 2020
@@ -25,4 +24,4 @@ using namespace boost::multiprecision;
  * "T factorial(unsigned i, const Policy&); [with T = cpp_dec_float_100]"
  * Code from boost/math/special_functions/factorials.hpp
  */
-mp_float A=13591409,B=545140134,C=640320,D=1.5,E=1,(*f)(int)=[](int n){return n<2?1:n*f(n-1);},G=0,H;int main(){for(int k=0;k<7;k++){G+=(pow(-1,k)*f(6*k)*(A+B*k))/(f(3*k)*pow(f(k),3)*pow(C,3*k+D));}cout<<(H=E/(12*G)).str(100);}
+mp_float A=13591409,B=545140134,C=640320,(*f)(int)=[](int n){return n<2?1:n*f(n-1);},D=0,E;int main(){for(int k=0;k<7;k++){D+=(pow(-1,k)*f(6*k)*(A+B*k))/(f(3*k)*pow(f(k),3)*pow(C,3*k+1.5));}cout<<(E=1/(12*D)).str(100);}
